8cee4fb3d3fa44154fd82ffa11426895e428dade
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-08-19 22:23:32 -0700

    background

--- svg/src/clubday/app.ts
+++ svg/src/clubday/app.ts
@@ -1,3 +1,6 @@
+import * as SVG from "@svgdotjs/svg.js";
+import Background from "./bg";
+
 /**
  * 'main' method that runs the app
  */
@@ -5,4 +8,43 @@ export const run = () =>
 {
     // ...
     console.log("world, hello");
+
+    let bg = new Background();
 }
+
+/**
+ * get the total width of the webpage
+ */
+export const width = () =>
+{
+    return Math.max
+    (
+        document.body.scrollWidth,
+        document.body.offsetWidth, 
+        document.documentElement.clientWidth,
+        document.documentElement.scrollWidth,
+        document.documentElement.offsetWidth,
+    );
+}
+
+/**
+ * get the total height of the webpage
+ */
+export const height = () =>
+{
+    return Math.max
+    (
+        document.body.scrollHeight,
+        document.body.offsetHeight, 
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight,
+    );
+}
+
+/**
+ * the top-most SVG element in this website
+ */
+export const canvas = new SVG.Svg()
+    .addTo("#app")
+    .size("100%", "100%");
---
+++ svg/src/clubday/bg.ts
@@ -0,0 +1,70 @@
+import * as SVG from "@svgdotjs/svg.js";
+import { canvas, width, height } from "./app";
+
+export default class Background
+{
+    /**
+     * fraction of screen where the left half of background ends, and
+     * the right begins
+     */
+    private fraction: number;
+
+    /**
+     * left box
+     */
+    private lt: SVG.Rect;
+    /**
+     * right box
+     */
+    private rt: SVG.Rect;
+
+    constructor()
+    {
+        this.fraction = 0.5;
+
+        // retreive the webview size
+        const w = width();
+        const h = height();
+
+        this.lt = canvas
+            .rect(w * 0.5, h)
+            .x(0)
+            .y(0)
+            .fill('red');
+        this.rt = canvas
+            .rect(w * 0.5, h)
+            .x(w * 0.5)
+            .y(0);
+
+        // resize dynamically
+        window.addEventListener('resize', e =>
+        {
+            console.log('resize...');
+
+            this.resize(this.fraction, 0.0);
+        });
+    }
+
+    public resize(frac: number, duration = 0.4): void
+    {
+        // retreive the webview size
+        const w = width();
+        const h = height();
+
+        // immediately scale height
+        this.lt.height(h);
+        this.rt.height(h);
+
+        // animate
+        this.lt
+            .animate(duration, '-', duration)
+            .size(w * frac, h);
+        this.rt
+            .animate(duration, '-', duration)
+            .x(w * frac)
+            .size(w * ( 1 - frac), h);
+
+        // update cached frac
+        this.fraction = frac;
+    }
+}
