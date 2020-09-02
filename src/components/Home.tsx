ecf32ce2d54449bf5e5f630fadab7ebe48dc2dc5
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-09-02 16:44:48 -0700

    added concept files

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const Home: React.FC = () =>
 (
-    <h1 style={{textAlign: "center"}}>
+    <h1 style={{ textAlign: "center" }}>
         los altos club day
     </h1>
 );
