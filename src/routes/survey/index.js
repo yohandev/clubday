c9dede9a2340975ca68cb1c1ffc82ec0110aa322
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-09-04 15:26:32 -0700

    draggable list

--- src/routes/survey/index.js
+++ src/routes/survey/index.js
@@ -1,4 +1,5 @@
 import m from "mithril" // this line is important! even if vscode detects this as dead code
+import Sortable from "sortablejs"
 
 /**
  * survey route
@@ -12,10 +13,19 @@ const Survey =
             <button>continue</button>
 
             <div id="survey-list">
-                
+                <ul id="survey-list-table">
+                    <li>Move</li>
+                    <li>Or drag</li>
+                    <li>Each of the items</li>
+                    <li>To different positions</li>
+                </ul>
             </div>
         </div>
-    )
+    ),
+    oncreate: () =>
+    {
+        Sortable.create(document.getElementById("survey-list-table"))
+    }
 }
 
 export default Survey;
