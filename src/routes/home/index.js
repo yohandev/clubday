6e679201e0df95389f6607aa77d10d3c84d85f0f
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-09-04 14:46:53 -0700

    added concepts + generalized logo style

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,7 +8,7 @@ const Home =
     view: () =>
     (
         <div id="home-body">
-            <h1 id="home-logo">los altos <span data-text="club" id="home-logo-club">club</span> day</h1>
+            <h1 id="home-logo" class="title">los altos <span data-text="club" class="title-reversed">club</span> day</h1>
             <a href="#!/survey">
                 <button id="home-button" class="heavy-button">start</button>
             </a>
