61fa3dc14946906b7214ea6c7b78273c4a34257c
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-09-06 01:58:07 -0700

    request clubs via xmlhttprequest

--- src/routes/clubs/index.js
+++ src/routes/clubs/index.js
@@ -1,4 +1,5 @@
 import m from "mithril" // this line is important! even if vscode detects this as dead code
+import CSV from "comma-separated-values"
 
 import Club from "./club";
 
@@ -7,16 +8,52 @@ import Club from "./club";
  */
 const Clubs =
 {
+    database: [],
     view: () =>
     (
         <div id="clubs-body">
             <h1 id="clubs-title" class="title"><span data-text="clubs" class="title-reversed">clubs</span> &nbsp;for you!</h1>
         
             <div id="clubs-list">
-                <Club name="green team" description="lorem ipsum" keywords={["helping others", "environment"]} />
+            {
+                Clubs.database.map((club) =>
+                (
+                    <Club name={ club.name || "" } description="desc" keywords={ club.keywords || [] } />
+                ))
+            }
             </div>
         </div>
-    )
+    ),
+    oninit: () =>
+    {
+        let req = new XMLHttpRequest();
+        
+        req.addEventListener("load", (data) =>
+        {
+            // retrieve the .csv as a string
+            const txt = data.currentTarget.responseText;
+
+            // parse .csv
+            Clubs.database = new CSV(txt, { header: true }).parse();
+
+            // parse keywords
+            for (var i = 0; i < Clubs.database.length; i++)
+            {
+                Clubs.database[i].keywords = Clubs.database[i].keywords
+                    .split(",")
+                    .map((s) => s.trim())
+            }
+
+            // redraw
+            m.redraw()
+        })
+        req.addEventListener("error", (err) =>
+        {
+            console.error(err)
+        })
+        req.open("GET", require("../../../assets/clubs.csv"))
+        req.send()
+    },
 }
 
 export default Clubs;
