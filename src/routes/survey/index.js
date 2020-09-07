9364cab964a5ad8dc4e21fbb7d8d4b3e62a3e821
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-09-07 15:06:27 -0700

    changed to "select your interests"

--- src/routes/survey/index.js
+++ src/routes/survey/index.js
@@ -27,9 +27,9 @@ const Survey =
     view: () =>
     (
         <div id="survey-body">
-            <h1 id="survey-title" class="title">sort your &nbsp;<span data-text="interests" class="title-reversed">interests</span></h1>
+            <h1 id="survey-title" class="title">select your &nbsp;<span data-text="interests" class="title-reversed">interests</span></h1>
 
-            <h1 id="survey-subtitle" class="title">(drag & drop the keywords below)</h1>
+            <h1 id="survey-subtitle" class="title">(scroll down to see all keywords)</h1>
             <h1 id="survey-footer" class="title">...then, press</h1>
             <a href="#!/clubs">
                 <button id="survey-button" class="heavy-button">continue</button>
