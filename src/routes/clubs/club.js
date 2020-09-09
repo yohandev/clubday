e620ab075bbc3d9a20692ada60d7f1577217b2a7
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-09-09 15:04:52 -0700

    lacoin tag tooltip

--- src/routes/clubs/club.js
+++ src/routes/clubs/club.js
@@ -12,7 +12,17 @@ const Club =
 
             <div class="clubs-list-item-tags-list">
                 { vnode.attrs.keywords.map((keyword) => <div class="clubs-list-item-tags-list-item">{ keyword }</div>) }
-                { vnode.attrs.lacoin ? <div class="clubs-list-item-tags-list-item">LACoin</div> : undefined }
+                {
+                    vnode.attrs.lacoin ?
+                    <a href="https://docs.google.com/document/d/1SBuS_xbCK1BCOG-dEDVL0K8V8492HG_F564zkjOFu3Y/edit" target="_blank" style="text-decoration: none; color: inherit">
+                        <div class="clubs-list-item-tags-list-item tooltip">
+                            LACoin&nbsp;
+                            <i class="fa fa-info-circle"></i>
+                            <span class="tooltiptext">This club supports LACoin. <span class="pseudo-link">Learn more</span></span>
+                        </div>
+                    </a>
+                    : undefined
+                }
             </div>
 
             <p> { vnode.attrs.description }</p>
