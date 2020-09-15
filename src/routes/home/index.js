9f320bcb4562302371844077f5f6ac7042ed51f0
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-09-14 20:54:46 -0700

    removed motd

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -11,10 +11,10 @@ const Home =
     view: () =>
     (
         <div id="home-body">
-            <Motd
+            {/* <Motd
                 message="⚠️ our clubs list isn't finalized yet, and may contain some unapproved clubs. ⚠️"
                 details="(please come back during club day on 09/16/2020)"
-            />
+            /> */}
 
             <h1 id="home-logo" class="title">los altos <span data-text="club" class="title-reversed">club</span> day</h1>
             <a href="#!/survey">
